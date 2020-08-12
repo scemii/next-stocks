b28ba10d7824338499c2b15e93a4a3ea30edde15
scemii <pierre_raffalli@msn.com>
2020-08-12 23:06:52 +0200

    adding head

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Head from "next/head";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import axios from "axios";
@@ -28,36 +29,40 @@ export default function Home() {
   }, 180000);
 
   return data?.length > 0 ? (
-    <Container maxWidth="lg">
-      <h1>Stocks App</h1>
-      <Grid id="grid-global" container spacing={3} justify="center">
-        {data.map((element) => (
-          <Grid item xs={12} sm={4}>
-            <SimpleCard
-              name={element.stockName}
-              isin={element.isin}
-              secteur={element.secteur}
-              stockValue={element.stockValue}
-              variation={element.variation}
-              opening={element.opening}
-              closing={element.closing}
-              volume={element.volume}
-            />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Head>
+      <Container maxWidth="lg">
+        <h1>Stocks App</h1>
+        <Grid id="grid-global" container spacing={3} justify="center">
+          {data.map((element) => (
+            <Grid item xs={12} sm={4}>
+              <SimpleCard
+                name={element.stockName}
+                isin={element.isin}
+                secteur={element.secteur}
+                stockValue={element.stockValue}
+                variation={element.variation}
+                opening={element.opening}
+                closing={element.closing}
+                volume={element.volume}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </Head>
   ) : (
-    <Container
-      maxWidth="lg"
-      style={{
-        top: "50%",
-        left: "50%",
-        position: "absolute",
-        fontSize: "1.5em",
-      }}
-    >
-      Loading...
-    </Container>
+    <Head>
+      <Container
+        maxWidth="lg"
+        style={{
+          top: "50%",
+          left: "50%",
+          position: "absolute",
+          fontSize: "1.5em",
+        }}
+      >
+        Loading...
+      </Container>
+    </Head>
   );
 }
